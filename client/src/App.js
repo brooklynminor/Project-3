b81a62ede03e454b444dbf6721bb29a0e83fa3cc
whiteowl00 <70918768+whiteowl00@users.noreply.github.com>
2020-11-26 00:08:54 -0500

    worked on Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,38 @@
 import React from 'react';
 import './App.css';
 
+import { Button, Card, Form } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
       <header class="App-header">
-        <h1>Pet Hotel</h1>
+        <h1 class="col-sm-8">Pet Hotel</h1>
+        <p class="col-sm-2">Contact</p>
+        <p class="col-sm-2">About Us</p>
+
       </header>
 
-      <body class="App-body">
-        <card>
-          <card.img src="" />
-          <card.body>
-            <card.title></card.title>
-            <card.text></card.text>
-            <button>Login</button>
-          </card.body>
-        </card>
-        <p>Body</p>
-      </body>
+      <div class="App-body">
+        <div class="card">
+          <Card.Img src="https://wtop.com/wp-content/uploads/2016/12/ThinkstockPhotos-509640879.jpg" style={{width:550}}/>
+          <Card.Body>
+            <Card.Title>
+              Welcome to PET HOTEL
+            </Card.Title>
+            <Form>
+              <Form.Control type="email" placeholder="ID : Email"/>
+              <Form.Control type="password" placeholder="Password" style={{marginTop:10}}/>
+            </Form>
+            
+          </Card.Body>
+          <Button>Login</Button>
+        </div>
+      </div>
 
-      <footer class="App-footer">
-        <p>Project Team Name</p>
-      </footer>
+      <div class="App-footer">
+        <p>@Project Team Name</p>
+      </div>
     
     </div>
   );
