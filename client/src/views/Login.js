f4934178ebb71d13acef1dabee92e94a9b6906d5
whiteowl00 <70918768+whiteowl00@users.noreply.github.com>
2020-11-26 18:33:30 -0500

    added files

---
+++ client/src/views/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from "./components/Card";
+import LoginForm from "./components/LoginForm";
+import { Button, Card, Form } from 'react-bootstrap'
+
+
+      <div class="row">  
+      <div class="col-md-6">&nbsp;</div>
+        <div class="col-md-6">
+          <div class="card">
+          <Card.Img src="https://wtop.com/wp-content/uploads/2016/12/ThinkstockPhotos-509640879.jpg" />
+          <Card.Body>
+            <Card.Title>
+              Welcome to PET HOTEL
+            </Card.Title>
+            <Form>
+              <Form.Control type="email" placeholder="ID : Email"/>
+              <Form.Control type="password" placeholder="Password" style={{marginTop:10}}/>
+              <Form.Text class="fs-6 text-muted">
+                If you don't have ID yet, Please join us<a href="#"> Now.</a> 
+              </Form.Text>
+            </Form>
+            
+          </Card.Body>
+          <Button>Login</Button>
+          </div>
+        </div>
+        </div>
+      
---
+++ client/src/views/NavBar2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../App.css'
+
+<header class="App-header">
+<a href="#" class="col-sm-8 fs-1">Pet Hotel</a>
+<a href="#" class="col-sm-2 ml-2 mr-1">Contact</a>
+<a href="#" class="col-sm-2 ml-1">About Us</a>
+
+</header>
