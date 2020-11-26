5891a06354751d34f647016759b019886971339d
whiteowl00 <70918768+whiteowl00@users.noreply.github.com>
2020-11-25 19:44:31 -0500

    just little touch on css and html

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header class="App-header">
+        <h1>Pet Hotel</h1>
       </header>
+
+      <body class="App-body">
+        <card>
+          <card.img src="" />
+          <card.body>
+            <card.title></card.title>
+            <card.text></card.text>
+            <button>Login</button>
+          </card.body>
+        </card>
+        <p>Body</p>
+      </body>
+
+      <footer class="App-footer">
+        <p>Project Team Name</p>
+      </footer>
+    
     </div>
   );
 }
