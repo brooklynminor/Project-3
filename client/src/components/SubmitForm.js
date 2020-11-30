5927860f4949d36c585b0b25185b04dd4b8ecb54
Brooklyn Minor <brooklynminor4@gmail.com>
2020-11-30 18:02:17 -0500

    register page updated

--- client/src/components/SubmitForm.js
+++ client/src/components/SubmitForm.js
@@ -40,35 +40,19 @@ class RegisterPet extends Component {
   }
   render() {
   return(
-<<<<<<< HEAD
-    <div className="container-md mt-5" style={{ width: "60%"}}>
-      <div className="display-4 mb-3 text-centre" style={{fontFamily: 'Oswald', color: "#ffce00", textAlign: 'center',}}>Register Your Pet</div>
-    <form onSubmit={this.handleSubmit} className="form-group" style={{marginTop: "50px"}}>
-      <div className="form-group">
-        <label className="form-group__Label" htmlFor="name">Pets Name: </label>
-        <input type="text" className="form-control" placeholder="Enter pets name..." name="petName" value={this.state.petName} onChange={this.handleChange} />
-      </div>
-      <div className="form-group">
-        <label className="form-group__Label" htmlFor="phone">Breed: </label>
-        <input type="text" id="breed" className="form-control" placeholder="Enter breed of pet..." name="breed" value={this.state.breed} onChange={this.handleChange} />
-      </div>
-=======
     <div className="container mt-3">
       <div className="row">
           <div className="col col-md-12 col-xl-10 col-lg-8 col-sm-6">
-
-<<<<<<< HEAD
-              <div className="display-4 mb-3">Register Your Pet</div>
-            <form onSubmit={this.handleSubmit} className="form-group">
+          <div className="display-4 mb-3 text-centre" style={{fontFamily: 'Oswald', color: "#ffce00", textAlign: 'center',}}>       
+          <form onSubmit={this.handleSubmit} className="form-group" style={{marginTop: "50px"}}>
               <div className="form-group">
                 <label className="form-group__Label" htmlFor="name">Pets Name: </label>
-                <input type="text" id="petName" className="form-control" placeholder="Enter your full name" name="petName" value={this.state.petName} onChange={this.handleChange} />
+                <input type="text" id="petName" className="form-control" placeholder="Enter pets name..." name="petName" value={this.state.petName} onChange={this.handleChange} />
               </div>
               <div className="form-group">
                 <label className="form-group__Label" htmlFor="phone">Breed: </label>
                 <input type="text" id="breed" className="form-control" placeholder="Enter your phone number" name="breed" value={this.state.breed} onChange={this.handleChange} />
               </div>
->>>>>>> 07f7cb3793ef47e0c2cdb2a08f5e96ebf3f0b2a1
 
               <div className="form-group">
                 <label className="form-group__Label" htmlFor="password">Size: </label>
@@ -83,18 +67,7 @@ class RegisterPet extends Component {
               </div>
             </form>
             </div>
-=======
-      <div className="form-group">
-        <label className="form-group__Label" htmlFor="password">Size: </label>
-        <input type="size" className="form-control" placeholder="Enter size of pet..." name="size" value={this.state.size} onChange={this.handleChange} />
-      </div>
-      <div className="form-group">
-        <label className="form-group__Label" htmlFor="email">Special Requirements: </label>
-        <textarea type="note" className="form-control" placeholder="Enter any medical/special attention required..." name="note" value={this.state.note} onChange={this.handleChange} />
-      </div>
-      <div className="form-group">
-          <button className="btn btn-primary mr-3 form-Button mr-20" onClick={this.handleSubmit}>Register</button>
->>>>>>> 06200198fb797c84cb8a8d740c4a72939c3e851d
+          </div>
       </div>
   </div>
 );
